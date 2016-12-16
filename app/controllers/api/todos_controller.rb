76c9d660a1cdbe895737f08f782e828c84acad9b
Rafael Monteiro <rafaelmonteiroti@gmail.com>
2016-12-16 11:29:20 -0300

    Revert "add racks-cors"
    This reverts commit 8a1cc16a52ec2b148f9fec706657ecbcc4df0c61.

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -2,55 +2,7 @@ module Api
   class TodosController < ApplicationController
 
     def index
-      @todos = Todo.all.order("completed ASC").order("id DESC")
-      render json: @todos
-    end
-
-    def show
-      @todo = Todo.find(params[:id])
-      render json: @todo
-    end
-
-    def new
-      @todo = Todo.new
+      @todos = Todos.all
     end 
-    
-    def edit
-    end 
-
-    def create
-      @todo = Todo.new(todo_params)
-      if @todo.save
-        render :json => @todo, status: 201
-      else 
-        render :json => { errors: @todo.errors.messages }, status: 422
-      end
-    end
-
-    def update
-      @todo = Todo.find(params[:id])
-      if @todo.update(todo_params)
-        render :json => @todo, status: 201
-      else
-        render :json => { errors: @todo.errors.messages }, status: 422
-      end  
-    end
-
-    def destroy
-      @todo = Todo.destroy(params[:id])
-      if @todo.destroy
-        render :json => { :head => "ok" }
-      else 
-        render :json => { errors: @todo.errors.messages}, status: 422
-      end 
-    end  
-
-
-    private
-
-    def todo_params
-      params.require(:todo).permit(:title, :completed)
-    end
-
   end
 end 
